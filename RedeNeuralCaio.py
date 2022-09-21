381822fd10f1da05b16f55a5625be299fc91f66b
luizgadel <lpgds.eng16@uea.edu.br>
2022-09-21 19:07:53 -0400

    Plots desempenho da RNCaio

--- RedeNeuralCaio.py
+++ RedeNeuralCaio.py
@@ -20,7 +20,9 @@ class NeuralNetwork:
     def sigmoid_prime(self, s):
         return self.sigmoid(s) * (1 - self.sigmoid(s))
         
-    def feed_forward(self, X):
+    def feed_forward(self, X, Y):
+        self.X = X
+        self.Y = Y
         #foward propagation through the network
         dot_product_A_1 = np.dot(X, self.W1) #dot product of X (input) and first set of weights (7x2)
         self.A_1 = self.sigmoid(dot_product_A_1) #activation function
@@ -29,26 +31,19 @@ class NeuralNetwork:
         self.A_2 = self.sigmoid(dot_product_A_2)
         return self.A_2
     
-    def backward_propagation(self, X, Y):
+    def backward_propagation(self):
         #backward propagate through the network
-        self.ol_error = Y - self.A_2 #error in output
+        self.ol_error = self.Y - self.A_2 #error in output
         self.delta_2 = self.ol_error * self.sigmoid_prime(self.A_2)
         
         self.hl_error = np.dot(self.delta_2, self.W2.T) #hl error: how much our hidden layer weights contribute to output error
         self.delta_1 = self.hl_error * self.sigmoid_prime(self.A_1) #applying derivative of sigmoid to A_1 error
         
-        self.W1 += np.dot(X.T, self.delta_1) #adjusting first set (input -> hidden) weights
+        self.W1 += np.dot(self.X.T, self.delta_1) #adjusting first set (input -> hidden) weights
         self.W2 += np.dot(self.A_1.T, self.delta_2) #adjusting second set (hidden -> output) weights
-        
-    def update_mini_batc(self, X, Y):
-        self.feed_forward(X)
-        self.backward_propagation(X, Y)
 
-    def train(self, X, Y):
-        self.X = X
-        self.Y = Y
-        self.outputs = []
-        for x, y in zip(X, Y):
-            output = self.feed_forward(x)
-            self.outputs.append(output)
-            # backprop(x, y)
+    def cost(self):
+        square_error = np.square(self.Y - self.A_2)
+        return np.mean(square_error), square_error
+
+    
